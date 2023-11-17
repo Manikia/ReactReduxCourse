49eaf6f5f222f6790ea060afe20a06a7d354603d
Monika <56901904+Manikia@users.noreply.github.com>
2023-11-16 20:00:13 -0800

    appended weather

--- src/outputWeather.js
+++ src/outputWeather.js
@@ -1,73 +1,61 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import getCityWeather from "./getCityWeather";
 import './outputCitiesStyling.css'
 
 const OutputWeather = ({ cityName, unit }) => {
     const [cityData, setCityData] = useState()
+    const [cityList, setCityList] = useState([])
 
-    useEffect(()=>{
+
+    useEffect(() => {
         const fetchData = async () => {
             const response = await getCityWeather(cityName, unit)
             setCityData(response)
+            if (response != null) {
+                setCityList([ // with a new array
+                    ...cityList, response // and one new item at the end
+                ])
+            }
         }
         fetchData()
     }, [cityName, unit]);
 
-    
+    // useEffect(()=>{
+    //     const fetchList = async () => {
+    //         <div className="cities">
 
-    if (!cityData){
+    //         <h1>location: {cityData.location}</h1>
+    //         <h1>current Temperature: {cityData.currentTemp}</h1>
+    //         <h1>Min Temperature: {cityData.tempMin}</h1>
+    //         <h1>Max Temperature: {cityData.tempMax}</h1>
+    //         <h1>Humidity: {cityData.humid}</h1>
+    //         </div>
+    //     }
+    //     // fetchList()
+    // }, []);
+
+
+
+    if (!cityData) {
         return (
             <h1>Theres no city for you, pal.</h1>
         )
     }
-    else{
+    else {
         return (
+            cityList.map((cityBox) =>
             <div className="cities">
-            
-            <h1>location: {cityData.location}</h1>
-            <h1>current Temperature: {cityData.currentTemp}</h1>
-            <h1>Min Temperature: {cityData.tempMin}</h1>
-            <h1>Max Temperature: {cityData.tempMax}</h1>
-            <h1>Humidity: {cityData.humid}</h1>
+                <h1>location: {cityBox.location}</h1>
+                <h1>current Temperature: {cityBox.currentTemp}</h1>
+                <h1>Min Temperature: {cityBox.tempMin}</h1>
+                <h1>Max Temperature: {cityBox.tempMax}</h1>
+                <h1>Humidity: {cityBox.humid}</h1>
             </div>
+          )
+            
         )
     }
 
-    // useEffect(() => {
-    //     return(
-    //         // if (cityName === undefined) {
-    //         <h1>Uh buddy, there is no city.</h1>
-    //         // }
-
-    //         // else {
-    //         //     const result =  getCityWeather(cityName)
-
-    //         //     <h1>here is my city: {result}</h1>
-
-
-    //         // }
-    //     )
-    // }) [cityName===undefined? "":cityName];
-
-    // const result =  getCityWeather(cityName)
-    //     return(
-    //         <h1>here is my city: {result}</h1>
-    //     )
-    // const cityData =  await getCityWeather(cityName)
-    // console.log(cityData)
-    // return(cityData.location)
-
-    // const showWeather = () => {
-    //     const [city, setCity] = useState([]);
-    //     useEffect(() => {
-    //         async function outputWeather(cityName){
-    //             const cityData =  await getCityWeather(cityName)
-    //             console.log(cityData)
-    //         return(cityData.location)
-    //         }
-    //         outputWeather();
-    //     }, []);
-    // }
 }
 
 export default OutputWeather;
